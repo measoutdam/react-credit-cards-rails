507cdb561f7ca1b725eef4f10c719fb005527f21
Outdam Meas <mstdam@gmail.com>
2018-03-08 08:36:21 +0700

    Added error stylesheet

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
+import moment from "moment"
+import Datetime from "react-datetime"
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+        <Datetime />
       </React.Fragment>
     );
   }
